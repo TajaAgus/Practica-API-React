79e5fa22d9ce761ebbc51a3e7fbe820f277daf3f
AgusTaja <agustin34tajani@gmail.com>
2023-09-25 15:13:27 -0300

    fix footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,18 @@ import Header from "./Components/Header/Header";
 import Search from "./Components/Search/Search";
 import AllCharacters from "./Components/AllCharacters/AllCharacters";
 import Footer from "./Components/Footer/Footer";
+import "./app.css" 
 
 function App() {
   return (
-    <>
+    <div className="app">
     <Header/>
     <Routes>
       <Route path="/" element={<Search/>}/>
       <Route path="/personajes" element={<AllCharacters/>}/>
     </Routes>
     <Footer/>
-    </>
+    </div>
     
   );
 }
